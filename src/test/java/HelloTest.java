62911a1ff9a081500efcc785ec0ffd7efb509004
Serkan Kenc <skenc06@gmail.com>
2021-02-06 16:20:57 +0100

    doing more work

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -3,6 +3,7 @@ public class HelloTest {
     public static void main(String[] args) {
 
         System.out.println("Testing hello"); //after run, target file comes
+        //doing more work
 
     }
 }
